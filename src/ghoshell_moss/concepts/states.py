6575c7d8366c7cb31a8548ea9824451159b5311a
thirdgerb <thirdgerb@gmail.com>
2025-09-08 22:53:08 +0800

    feat: upgrade states and topic design

--- src/ghoshell_moss/concepts/states.py
+++ src/ghoshell_moss/concepts/states.py
@@ -9,7 +9,7 @@ class State(BaseModel):
     name: str = Field(description="The name of the state object.")
     description: str = Field(default="", description="The description of the state object.")
     schema: Dict[str, Any] = Field(description="the json schema of the state")
-    default: Dict[str, Any] = Field(description="the default value of the state")
+    data: Dict[str, Any] = Field(description="the default value of the state")
 
 
 class StateModel(BaseModel, ABC):
@@ -18,6 +18,8 @@ class StateModel(BaseModel, ABC):
     """
     state_desc: ClassVar[str] = ""
     state_name: ClassVar[str] = ""
+
+    version: str = Field(default="", description="state version, optimis lock")
 
     @classmethod
     def to_state(cls) -> State:
@@ -31,21 +33,37 @@ class StateModel(BaseModel, ABC):
 class StateStore(ABC):
 
     @abstractmethod
-    def register(self, state: State) -> None:
+    def register(self, *states: State | StateModel) -> None:
+        """
+        注册一个状态.
+        """
         pass
 
-    @abstractmethod
-    def register_model(self, model: StateModel) -> None:
+    def set(self, state: State | StateModel) -> None:
+        """
+        强制设置一个 State 到本地.
+        """
         pass
 
     @abstractmethod
     async def get(self, state_name: str) -> Dict[str, Any]:
+        """
+        获取当前状态. 只有注册过的状态才会返回值.
+        :raise AttributeError: 如果调用了没注册过的 State, 会抛出异常.
+        """
         pass
 
     @abstractmethod
-    async def save_model(self, model: StateModel) -> None:
+    async def get_model(self, default: StateModel) -> StateModel:
+        """
+        获取一个强类型的 StateModel. 如果目标不存在, 或者数据结构有冲突, 会返回 default 值.
+        """
         pass
 
     @abstractmethod
-    async def get_model(self, model: Type[StateModel]) -> StateModel:
+    async def save(self, state: StateModel | State) -> bool:
+        """
+        保存一个 State. 其中的 Version 是乐观锁.
+        Save 会触发广播和更新.
+        """
         pass
--- src/ghoshell_moss/concepts/topics.py
+++ src/ghoshell_moss/concepts/topics.py
@@ -1,23 +1,38 @@
 import anyio
 from pydantic import BaseModel, Field
-from typing import TypedDict, Dict, Any, ClassVar, Literal, Optional, Union, List, Callable, Type, Coroutine, TypeVar
+from typing import (
+    TypedDict, Dict, Any, ClassVar, Optional, Union, List, Callable, Type, Coroutine, Iterable, Protocol,
+)
 from typing_extensions import Self
 from abc import ABC, abstractmethod
 from ghoshell_common.helpers import generate_import_path, uuid
 
 
 class Topic(TypedDict):
+    """
+    在 channel 之间广播的数据结构.
+    不关心 topic broker 的通讯协议.
+    """
     id: str
+    """每个 topic 有唯一 id. """
+
     name: str
+    """topic 的类型 id"""
+
     issuer: str
-    batch_id: Optional[str]
-    complete: bool
+    """发布者的类型"""
+    issuer_id: str
+    """发布者的唯一 id. 这是假设有多个发布者的情况下. """
+
     req_id: Optional[str]
-    data: Dict[str, Any] | List | str | bool | float | int | bytes
+    """如果这个 topic 是对另一个 topic 的回复, 会携带那个 topic 的 id"""
+
+    data: Dict[str, Any] | List | str | bool | float | int | bytes | None
+    """ topic 的数据结构. 基本要求是传递标量. """
 
 
-def make_topic_prefix(name: str, issuer: str = "") -> str:
-    return f"{name}||{issuer}" if issuer else name
+def make_topic_prefix(name: str, issuer: str = "", issuer_id: str = "") -> str:
+    return f"{name}|{issuer}|{issuer_id}"
 
 
 class TopicMeta(TypedDict):
@@ -26,13 +41,41 @@ class TopicMeta(TypedDict):
     schema: Dict[str, Any]
 
 
-class TopicModel(BaseModel, ABC):
+class TopicModel(Protocol):
+
+    @classmethod
+    @abstractmethod
+    def get_topic_name(cls) -> str:
+        pass
+
+    @classmethod
+    @abstractmethod
+    def to_topic_meta(cls) -> TopicMeta:
+        pass
+
+    @classmethod
+    def from_topic(cls, topic: Topic) -> Self | None:
+        pass
+
+    @abstractmethod
+    def new_topic(self, req_id: Optional[str] = None) -> Topic:
+        pass
+
+
+class TopicBaseModel(BaseModel, ABC):
+    """
+    一种简单的方式快速定义出 topic.
+    """
+
     topic_name: ClassVar[str] = ""
     topic_description: ClassVar[str] = ""
 
+    # topic 保留的关键字.
+
     issuer: str = Field(default='', description='Issuer of the topic')
+    issuer_id: str = Field(default='', description='Issuer of the topic')
     req_id: Optional[str] = Field(default=None, description='the topic is response to topic id')
-    topic_id: str = Field(default_factory=uuid, description='the topic id')
+    id: str = Field(default_factory=uuid, description='the topic id')
 
     @classmethod
     def get_topic_name(cls) -> str:
@@ -51,9 +94,10 @@ class TopicModel(BaseModel, ABC):
         if topic["name"] != cls.get_topic_name():
             return None
         data = topic["data"]
-        data['issuer'] = topic["issuer"]
+        data['issuer'] = topic['issuer']
+        data['issuer_id'] = topic['issuer_id']
         data['req_id'] = topic.get('req_id', None)
-        data['tid'] = topic['id']
+        data['id'] = topic['id']
 
         model = cls(**data)
         return model
@@ -67,98 +111,44 @@ class TopicModel(BaseModel, ABC):
             id=tid,
             name=self.get_topic_name(),
             issuer=issuer,
+            issuer_id=self.issuer_id,
             data=data,
             req_id=req_id,
         )
 
 
-TopicModelType = TypeVar('TopicModelType', bound=TopicModel)
+TopicCallback = Union[Callable[[Topic], Coroutine[None, None, None]] | Callable[[Topic], None]]
+TopicModelCallback = Union[Callable[[TopicModel], Coroutine[None, None, None]] | Callable[[TopicModel], None]]
 
 
-class TopicDispatcher(ABC):
-    issuer: str
+class Topics(ABC):
 
     @abstractmethod
-    async def send(self, topic: Topic) -> None:
+    def on(self, topic_name: str, callback: TopicCallback) -> None:
         pass
 
     @abstractmethod
-    async def send_model(self, topic_model: TopicModel) -> None:
+    def on_model(self, topic_model: Type[TopicModel], callback: TopicModelCallback) -> None:
         pass
 
     @abstractmethod
-    async def request(
-            self,
-            req: TopicModel,
-            resp_type: Type[TopicModelType],
-            timeout: float | None = None,
-    ) -> TopicModelType:
-        pass
-
-
-TopicCallback = Callable[[Topic], Coroutine[None]]
-TopicModelCallback = Callable[[TopicModel], Coroutine[None]]
-
-
-class TopicListener(ABC):
-
-    @abstractmethod
-    def on(self, topic_prefix: str, callback: TopicCallback) -> None:
-        pass
-
-    @abstractmethod
-    def on_model(self, topic_model: Type[TopicModel], callback: TopicModelCallback, issuer: str = "") -> None:
-        pass
-
-    @abstractmethod
-    def listening(self) -> List[str]:
-        pass
-
-
-class TopicTransport(ABC):
-    @abstractmethod
-    def listener(self) -> TopicListener:
-        pass
-
-    @abstractmethod
-    def dispatcher(self) -> TopicDispatcher:
-        pass
-
-    @abstractmethod
-    async def start(self) -> None:
+    def register(self, listening: Iterable[TopicMeta], sending: Iterable[TopicModel]) -> None:
         """
-        start to listen
+        注册本地可能的 topic 类型.
         """
         pass
 
     @abstractmethod
-    async def close(self) -> None:
+    async def send(self, topic: Topic | TopicModel) -> None:
+        """
+        发送一个 topic.
+        """
         pass
 
-    async def __aenter__(self):
-        await self.start()
-
-    async def __aexit__(self, exc_type, exc_val, exc_tb):
-        await self.close()
-
-
-class CommandToken(TopicModel):
-    topic_name: ClassVar[str] = "MOSShell.command_token"
-    topic_description: ClassVar[str] = ""
-
-    name: str = Field(description="command name")
-    cid: str = Field(description="command id")
-    chan: str = Field(description="channel name of the command")
-    type: Literal['start', 'delta', 'end'] = Field(description="command token type")
-    content: str = Field(description="the origin tokens")
-    args: Optional[Dict[str, Any]] = Field(None, description="the command arguments")
-
-
-class CommandResult(TopicModel):
-    topic_name: ClassVar[str] = "MOSShell.command_result"
-    topic_description: ClassVar[str] = ""
-
-    cid: str = Field(description="command id")
-    errcode: int = Field(0, description="command error code")
-    errmsg: str = Field("", description="command error message")
-    result: Union[list, bool, str, dict, None] = Field(None, description="the result of the command")
+    @abstractmethod
+    async def recv(self, timeout: float | None = None) -> Topic:
+        """
+        获取一个被广播的 topic
+        :raise TimeoutError: 如果设置了 timeout.
+        """
+        pass
